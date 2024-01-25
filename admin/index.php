429bd1127b96c43b29793a39f18b241e7b9ae187
pranaylamsecode <pranaylamsecode@gmail.com>
2024-01-25 18:59:22 +0530

    added change

--- admin/index.php
+++ admin/index.php
@@ -234,6 +234,8 @@ switch (ENVIRONMENT)
 	// Path to the front controller (this file) directory
 	define('FCPATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
 
+
+
 	// Name of the "system" directory
 	define('SYSDIR', basename(BASEPATH));
 
